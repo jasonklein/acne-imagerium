499bde91e274446a03e287700f6c14f39370a054
jasonklein <jasonklein@gmail.com>
2014-08-31 16:49:22 +0100

    Refactor forms into a single form and add view helper.

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,2 +1,12 @@
 module ImagesHelper
+
+  def upload_field(f, upload_type)
+    case upload_type
+    when "local"
+      f.file_field :content
+    when "remote"
+      f.text_field :remote_content_url
+    end
+  end
+
 end
