2c6d5115741fdadca2f58b196b7bf813e1ee3769
jasonklein <jasonklein@gmail.com>
2014-08-31 14:52:08 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AcneImagerium::Application.config.secret_token = '55a6e2bf7fdaf4d3bc7a93bd02dfa3bd8ead82b35600992e2055776618cfad560e08b95727a27f4f6f04d5303feb156cf9c4791620c5f29813f53618a305e0dc'
