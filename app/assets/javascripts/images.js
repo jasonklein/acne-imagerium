5ff5237f5b1cc374eb30404cb112df2e841c8791
jasonklein <jasonklein@gmail.com>
2014-09-02 00:58:51 +0100

    Load all images in index view, but use javascript to randomly display one at start.

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,14 +1,21 @@
 var ImageriumApp = ImageriumApp || {};
 
-ImageriumApp.add_thumbnail_click_handlers = function() {
-  $(".image-thumbnail").each(function(i, thumbnail){
+ImageriumApp.selectRandomImageToDisplay = function(thumbnails) {
+  var thumbnail = thumbnails[Math.floor(Math.random() * thumbnails.length)];
+  var id = $(thumbnail).data("id");
+  $("#" + id).removeClass("hidden-image").addClass("visible-image");
+}
+
+ImageriumApp.add_thumbnail_click_handlers = function(thumbnails) {
+  $.each(thumbnails, function(i, thumbnail){
     var id = $(this).data("id");
-    
   });
 };
 
 ImageriumApp.setup = function() {
-  ImageriumApp.add_thumbnail_click_handlers();
+  var $thumbnails = $(".image-thumbnail");
+  ImageriumApp.add_thumbnail_click_handlers($thumbnails);
+  ImageriumApp.selectRandomImageToDisplay($thumbnails);
 }
 
 $(ImageriumApp.setup);
