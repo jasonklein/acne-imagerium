f6948be99aa5a87684e66fff96e5974514b8a8ce
jasonklein <jasonklein@gmail.com>
2014-09-03 09:36:59 +0100

    Attach click handlers to prev/next buttons.

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -2,22 +2,37 @@ var ImageriumApp = ImageriumApp || {};
 
 ImageriumApp.selectRandomImageToDisplay = function(thumbnails) {
   var thumbnail = thumbnails[Math.floor(Math.random() * thumbnails.length)];
-  var id = $(thumbnail).data("id");
-  var imageWrapper = $("#" + id).parent()
-  var imageViewWrapper = imageWrapper.parent();
-  imageViewWrapper.removeClass("hidden-image").addClass("visible-image");
+  var imageId = $(thumbnail).data("id");
+  ImageriumApp.reveal_image(imageId);
 }
 
-ImageriumApp.add_thumbnail_click_handlers = function(thumbnails) {
+ImageriumApp.addThumbnailClickHandlers = function(thumbnails) {
   $.each(thumbnails, function(i, thumbnail){
     var id = $(this).data("id");
   });
 };
 
+ImageriumApp.addArrowClickHandler = function(arrowClass) {
+  $(arrowClass).click(function() {
+    var imageId = $(this).data("id");
+    console.log(imageId);
+    
+
+  });
+}
+
+ImageriumApp.reveal_image = function(imageId) {
+  var imageWrapper = $("#" + imageId).parent()
+  var imageViewWrapper = imageWrapper.parent();
+  imageViewWrapper.removeClass("hidden-image").addClass("visible-image");
+}
+
 ImageriumApp.setup = function() {
   var $thumbnails = $(".image-thumbnail");
-  ImageriumApp.add_thumbnail_click_handlers($thumbnails);
+  ImageriumApp.addThumbnailClickHandlers($thumbnails);
   ImageriumApp.selectRandomImageToDisplay($thumbnails);
+  ImageriumApp.addArrowClickHandler(".direction-arrow-prev");
+  ImageriumApp.addArrowClickHandler(".direction-arrow-next");
 }
 
 $(ImageriumApp.setup);
