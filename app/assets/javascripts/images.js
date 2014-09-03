138cc52d830e724ce4474e3ee27ee363ade69266
jasonklein <jasonklein@gmail.com>
2014-09-03 09:58:48 +0100

    Add hide image functionality.

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -3,7 +3,7 @@ var ImageriumApp = ImageriumApp || {};
 ImageriumApp.selectRandomImageToDisplay = function(thumbnails) {
   var thumbnail = thumbnails[Math.floor(Math.random() * thumbnails.length)];
   var imageId = $(thumbnail).data("id");
-  ImageriumApp.reveal_image(imageId);
+  ImageriumApp.revealImage(imageId);
 }
 
 ImageriumApp.addThumbnailClickHandlers = function(thumbnails) {
@@ -16,16 +16,24 @@ ImageriumApp.addArrowClickHandler = function(arrowClass) {
   $(arrowClass).click(function() {
     var imageId = $(this).data("id");
     console.log(imageId);
-    
+    ImageriumApp.changeImage(imageId);
 
   });
-}
+};
 
-ImageriumApp.reveal_image = function(imageId) {
-  var imageWrapper = $("#" + imageId).parent()
+ImageriumApp.revealImage = function(imageId) {
+  var imageWrapper = $("#" + imageId).parent();
   var imageViewWrapper = imageWrapper.parent();
   imageViewWrapper.removeClass("hidden-image").addClass("visible-image");
-}
+};
+
+ImageriumApp.hideImage = function() {
+  $(".visible-image").removeClass("visible-image").addClass("hidden-image");
+};
+
+ImageriumApp.changeImage = function(imageId) {
+  ImageriumApp.hideImage();
+};
 
 ImageriumApp.setup = function() {
   var $thumbnails = $(".image-thumbnail");
@@ -33,6 +41,6 @@ ImageriumApp.setup = function() {
   ImageriumApp.selectRandomImageToDisplay($thumbnails);
   ImageriumApp.addArrowClickHandler(".direction-arrow-prev");
   ImageriumApp.addArrowClickHandler(".direction-arrow-next");
-}
+};
 
 $(ImageriumApp.setup);
