bb46ff7ada84aca4d5db8a4fda857a833bdd954c
jasonklein <jasonklein@gmail.com>
2014-09-03 10:49:03 +0100

    Add and remove opacity for thumbnails with javascript; add ease-in-out animation for opacity change.

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -25,10 +25,13 @@ ImageriumApp.revealImage = function(imageId) {
   var imageWrapper = $("#" + imageId).parent();
   var imageViewWrapper = imageWrapper.parent();
   imageViewWrapper.removeClass("hidden-image").addClass("visible-image");
+  var thumbnail = $(".image-thumbnail[data-id=" + imageId);
+  thumbnail.removeClass("unselected-image-opacity").addClass("selected-image-opacity");
 };
 
 ImageriumApp.hideImage = function() {
   $(".visible-image").removeClass("visible-image").addClass("hidden-image");
+  $(".selected-image-opacity").removeClass("selected-image-opacity").addClass("unselected-image-opacity");
 };
 
 ImageriumApp.changeImage = function(imageId) {
